e2fb487708ad166a44378b8cde5b574910f8fd9b
Pankaj Kumar <pankajvitmca@gmail.com>
2023-04-09 12:19:48 +0530

    Click Popper Game-Instructor Version Completed

---
+++ Section_20_Click_Popper_Game/clickPopperInstructorVersion/script.js
@@ -0,0 +1,135 @@
+const playArea = {};
+const player = {};
+let gameObj;
+
+playArea.stats = document.querySelector(".stats");
+playArea.main = document.querySelector(".main");
+playArea.game = document.querySelector(".game");
+console.log(playArea);
+playArea.btns = Array.from(document.querySelectorAll(".btn"));
+playArea.page = Array.from(document.querySelectorAll(".page"));
+document.addEventListener("DOMContentLoaded", getData);
+
+playArea.btns.forEach(function (item) {
+  item.addEventListener("click", handleBtn);
+});
+
+function getData() {
+  playArea.main.classList.add("visible");
+  console.log("DOM loaded");
+  fetch("http://discoveryvip.com/shared/json.php?f=game")
+    .then(function (rep) {
+      return rep.json();
+    })
+    .then(function (data) {
+      gameObj = data.data;
+      buildBoard();
+      console.log(gameObj);
+    });
+}
+function updateScore() {
+  playArea.scorer.innerHTML =
+    "Score: " + player.score + " Lives:" + player.items;
+}
+
+function buildBoard() {
+  playArea.scorer = document.createElement("span");
+  playArea.scorer.innerHTML = "Press button to start";
+  playArea.stats.appendChild(playArea.scorer);
+  //debugger;
+  let rows = 4;
+  let cols = 4;
+  let cnt = 0;
+  playArea.game.style.width = cols * 100 + cols * 2 + "px";
+  playArea.game.style.margin = "auto";
+  for (let y = 0; y < rows; y++) {
+    let divMain = document.createElement("div");
+    divMain.setAttribute("class", "row");
+    divMain.style.width = cols * 100 + cols * 2 + "px";
+    for (let x = 0; x < cols; x++) {
+      let div = document.createElement("div");
+      div.setAttribute("class", "pop");
+      cnt++;
+      div.innerText = cnt;
+      div.cnt = cnt;
+      divMain.appendChild(div);
+    }
+    playArea.game.appendChild(divMain);
+  }
+}
+function handleBtn(e) {
+  if (e.target.classList.contains("newGame")) {
+    console.log("Yes");
+    startGame();
+  }
+}
+
+function startGame() {
+  player.score = 0;
+  player.items = 3;
+  playArea.main.classList.remove("visible");
+  playArea.game.classList.add("visible");
+  console.log("start");
+  player.gameOver = false;
+  startPop();
+  updateScore();
+}
+
+function randomUp() {
+  const pops = document.querySelectorAll(".pop");
+  const idx = Math.floor(Math.random() * pops.length);
+  if (pops[idx].cnt == playArea.last) {
+    return randomUp();
+  }
+  playArea.last = pops[idx].cnt;
+  return pops[idx];
+}
+function startPop() {
+  let newPop = randomUp();
+  console.log(newPop);
+  newPop.classList.add("active");
+  newPop.addEventListener("click", hitPop);
+  const time = Math.round(Math.random() * 1500 + 750);
+  const val = Math.floor(Math.random() * gameObj.length);
+  newPop.old = newPop.innerText;
+  newPop.v = gameObj[val].value;
+  newPop.innerHTML = gameObj[val].icon + "<br>" + gameObj[val].value;
+  playArea.inPlay = setTimeout(function () {
+    newPop.classList.remove("active");
+    newPop.removeEventListener("click", hitPop);
+    newPop.innerText = newPop.old;
+    if (newPop.v > 0) {
+      player.items--;
+      updateScore();
+    }
+    if (player.items <= 0) {
+      gameOver();
+    }
+
+    if (!player.gameOver) {
+      startPop();
+    }
+  }, time);
+}
+
+function gameOver() {
+  player.gameOver = true;
+  playArea.main.classList.add("visible");
+  playArea.game.classList.remove("visible");
+  document.querySelector(".newGame").innerText = "Try Again";
+}
+
+function hitPop(e) {
+  console.log(e.target.cnt);
+  console.log(e.target.v);
+  let newPop = e.target;
+  player.score = player.score + newPop.v;
+  updateScore();
+  newPop.classList.remove("active");
+  newPop.removeEventListener("click", hitPop);
+  newPop.innerText = newPop.old;
+  clearTimeout(playArea.inPlay);
+  if (!player.gameOver) {
+    startPop();
+  }
+}
