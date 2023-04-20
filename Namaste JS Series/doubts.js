38233da52789a57db94b73ae783e69e6b19f4ecc
Pankaj Kumar <pankajvitmca@gmail.com>
2023-04-20 08:48:56 +0530

    function expression and function statement and first class citizen concepts

---
+++ Namaste JS Series/doubts.js
@@ -0,0 +1,19 @@
+// What is constructor and how to made it through function ?
+// Example 
+
+function Counter() {
+            var count = 0;
+            this.incrementCounter = function() {
+                count++;
+                console.log(count);
+            }
+            this.decrementCounter = function() {
+                count--;
+                console.log(count);
+            }
+        }
+        var counter1 = new Counter();
+
+        counter1.incrementCounter();
+        counter1.incrementCounter();
+        counter1.decrementCounter();
